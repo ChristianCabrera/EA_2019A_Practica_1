926946684f4f6a44283ecff12e701e2a5d1c0597
ChristianCabrea <christianrco17@gmail.com>
2019-03-28 10:50:24 -0600

    practica1

---
+++ Sin Nombre1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+    #include <stdlib.h>
+    
+    struct Empleado;
+    char nombre[100];
+    char numeroDEEmpleado[10];
+    char numeroDeSEguro;
+};
+int main (){
+    struct Empleado empleados[1];
+
+    printf("ingrese el nombre");
+    scandf("%99"["\n)", empleados[0].nombre);
+    printf("ingrese la edad.");
+    scandf("%d", empleados[0].edad);
+    printf("ingrese el numero ")
+    scandf("%s"empleados[0].numeroDeEMple)
